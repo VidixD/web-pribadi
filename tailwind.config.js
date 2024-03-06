bbf2c1387245944b632f387514ec99b8a394c64d
vidigg <119675844+vidiggwp@users.noreply.github.com>
2024-03-06 22:11:38 +0700

    ea

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
   },
   plugins: [],
-}
-
+};
